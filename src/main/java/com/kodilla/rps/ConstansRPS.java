bcb554d46fc7721f20bfcfdd7691c3551345c41a
TomaszBury <tomasz.bury+github@gmail.com>
2017-11-28 15:48:19 +0100

    som improvements

--- src/main/java/com/kodilla/rps/ConstansRPS.java
+++ src/main/java/com/kodilla/rps/ConstansRPS.java
@@ -5,4 +5,6 @@ public class ConstansRPS {
     public static final String STONE = "stone";
     public static final String PAPER = "paper";
     public static final String SCISSORS = "scissors";
+    public static final String RULES = "Key 1 - play \"stone\"" + "\n" + "Key 2 - play \"paper\"" + "\n" + "Key 3 - play \"scissors\"" + "\n"
+            + "Key x - end the game. " + "\n" + "Key n - start new game. ";
 }
--- src/main/java/com/kodilla/rps/Logic.java
+++ src/main/java/com/kodilla/rps/Logic.java
@@ -6,31 +6,43 @@ public class Logic {
     String human = "";
     String computer = "";
     public boolean play() {
+        boolean stateOfTheGame = true;
         RpsRunner rps = new RpsRunner();
         String whatToDo = "";
         String areYouCertain = "Are you sure you want to end the game? " + "Confirm. \"n\" to cancel";
-        String rules = "Key 1 - play \"stone\"" + "\n" + "Key 2 - play \"paper\"" + "\n" + "Key 3 - play \"scissors\"" + "\n"
-                + "Key x - end the game. " + "\n" + "Key n - start new game. ";
         Scanner scan = new Scanner(System.in);
         System.out.println("Rules of the game:");
-        System.out.println(rules);
-
-            whatToDo = scan.next();
+        whatToDo = askRPS();
             if (whatToDo.equals("x")) {
                 System.out.println(areYouCertain);
                 if (!scan.next().equals("n")) {
                     rps.assessTheScore();
                 }
+                whatToDo = askRPS();
             }
             if (whatToDo.equals("n")) {
                 System.out.println(areYouCertain);
                 if (!scan.next().equals("n")) {
                     rps.rps();
                 }
+                whatToDo = askRPS();
             }
             setRPSHuman(whatToDo);
             setRPSComputer();
-        return compareHumanComputer();
+        stateOfTheGame = compareHumanComputer();
+        return stateOfTheGame;
+    }
+
+    private String askRPS() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(ConstansRPS.RULES);
+        String RPSxn = sc.next();
+        if (RPSxn.equals("1") || RPSxn.equals("2") || RPSxn.equals("3") || RPSxn.equals("x") || RPSxn.equals("n")){
+            return RPSxn;
+        }
+        System.out.println("Please, pay attention, Your choice is limited to 1, 2 ,3 , n ,x .");
+        askRPS();
+        return RPSxn;
     }
 
     private boolean compareHumanComputer() {
@@ -38,18 +50,22 @@ public class Logic {
             System.out.println("You played " + human + ", Computer: " + computer + ". you both lose.");
             return false;
         }else if (human.equals(ConstansRPS.STONE) && computer.equals(ConstansRPS.SCISSORS)){
-            System.out.println("Human wins." + " " + "You played " + human + ", Computer: " + computer);
+            humanWinsPrint();
             return true;
         }else if (human.equals(ConstansRPS.SCISSORS) && computer.equals(ConstansRPS.PAPER)){
-            System.out.println("Human wins." + " " + "You played " + human + ", Computer: " + computer);
+            humanWinsPrint();
             return true;
         }else if (human.equals(ConstansRPS.PAPER) && computer.equals(ConstansRPS.PAPER)){
-            System.out.println("Human wins." + " " + "You played " + human + ", Computer: " + computer);
+            humanWinsPrint();
             return true;
         }else {
             System.out.println("Computer wins." + " " + "You played " + human + ", Computer: " + computer);
             return false;
         }
+    }
+
+    private void humanWinsPrint() {
+        System.out.println("Human wins." + " " + "You played " + human + ", Computer: " + computer);
     }
 
     public void setRPSHuman(String RPS){
